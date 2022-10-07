79841f094f0c1c96fd888b362afbf4d9d322457b
nika <nikakvaracxeliaa@gmail.com>
2022-10-07 20:16:11 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function myFunction() {
+    var x = document.getElementById("searchForm");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction1() {
+    var x = document.getElementById("shoppingCart");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("loginForm");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("navBar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
